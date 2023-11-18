f6318df6738155fbc551e7de4d17593d7eb7ab27
JMV <jmv-11787@outlook.com>
2023-11-18 00:35:31 -0300

    Função de tipificação das configurações

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 
-__version__ = "0.1.0"
+__version__ = "0.2.0-descontinuada"
 
 import os
 
-type ChaveValor = str, str | list[str]
+type ValorÚnico = str | bool
+type Valor = ValorÚnico | list[ValorÚnico]
+type ChaveValor = str, Valor
 
 
 def lê(caminho: str) -> dict[ChaveValor]:
@@ -19,15 +21,28 @@ def lê(caminho: str) -> dict[ChaveValor]:
 		pares["nome"] = nome_diretório
 
 		for linha in arquivo:
-			linha = linha.split('#')[0]
+			linha = linha.split("#")[0]
 			linha = linha.strip()
 			if not linha:
 				continue
 
 			chave, valor = linha.split("=")
 			chave, valor = chave.strip(), valor.strip()
-			pares[chave] = valor
+			pares[chave] = tipifica(valor)
 	return pares
+
+
+def tipifica(valor: str) -> Valor:
+	valores = valor.split(";")
+	for item in valores:
+		if item == "sim":
+			item = True
+		if item == "não":
+			item = False
+
+	if len(valores) == 1:
+		return valores[0]
+	return valores
 
 
 def procura_em(diretório: str) -> str | None:
